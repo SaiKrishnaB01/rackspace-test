ca24f0d936a2a05c89555ff1834d6b2eedaebf97
saikrishna <bvssk89@gmail.com>
2020-03-22 16:35:13 +0530

    changes to remove duplicates from sorted integer array

---
+++ coding-test/src/service/RemoveDuplicates.java
@@ -0,0 +1,35 @@
+package service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicates {
+	
+	private static RemoveDuplicates instance = null;
+	
+	private RemoveDuplicates(){
+		
+	}
+	
+	public static RemoveDuplicates getInstance(){
+		if(instance == null)
+			instance = new RemoveDuplicates();
+		return instance;
+	}
+
+	public List<Integer> removeDuplicates(int[] sortedArray) {
+		int j=0;
+		for(int i=0; i<sortedArray.length-1; i++){
+			if(sortedArray[i] != sortedArray[i+1]){
+				sortedArray[j++] = sortedArray[i];
+			}
+		}
+		sortedArray[j++] = sortedArray[sortedArray.length - 1];
+		return Arrays.stream(sortedArray).boxed().collect(Collectors.toList()).subList(0, j);
+	}
+	
+
+}
+
+
